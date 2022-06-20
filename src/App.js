381228e8335c09dd65d877c595a0db22fc2dc467
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-06-20 21:20:19 +0900

    chapter3

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 
 function App() {
 
-  let [a,b] = useState('남자 코트 추천');
+  let [글제목, 글제목변경] = useState(['남자 코트 추천', '강남 우동맛집', '파이썬독학']);
 
   let posts = "강남 고기 맛집";
   return (
@@ -13,8 +13,18 @@ function App() {
         <div style={ { color: 'blue', fontSize : '30px'} }>개발 Blog</div>        
       </div>
       <div className='list'>      
-        <h3>{ posts }</h3>
+        <h3>{ 글제목[0] }</h3>
         <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>      
+        <h3>{ 글제목[1] }</h3>
+        <p>2월 18일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>      
+        <h3>{ 글제목[2] }</h3>
+        <p>2월 19일 발행</p>
         <hr/>
       </div>
       <div>
