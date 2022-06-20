519149c6859cfdb3f1e0a2486410b563fccda3ac
Seungjub-bob <rkdtmdwns0105@gmail.com>
2022-06-20 21:12:57 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  let [a,b] = useState('남자 코트 추천');
+
+  let posts = "강남 고기 맛집";
+  return (
+    <div className="App">
+      <div className="black-nav">
+        <div style={ { color: 'blue', fontSize : '30px'} }>개발 Blog</div>        
+      </div>
+      <div className='list'>      
+        <h3>{ posts }</h3>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
